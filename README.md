0e52e7e820bf14b98dab0400e2d10653ae169cc8
Bhavna Sharma <bhavna31sharma@gmail.com>
2022-11-20 15:25:12 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # learning-javascript
+
+# learning rest services
+
+- git clone https://github.com/SreedeviRajavelu/learning-javascript.git
+- git checkout basic-http-rest
+
+- pip install -r requirements.txt
+- uvicorn server:app --reload
+- python .\client.py
--- server.py
+++ server.py
@@ -11,6 +11,11 @@ def my_first_api():
 
 @app.get("/todos/{todo_id}")
 def todos(todo_id):
+    # Homework
+    # if todo_id is passed in the path, check if todo_id is in dictionary
+    # if todo_id is not in dictionary return []
+    # else return what is being returned now
+    # if todo_id is not passed, return whole dict
     if int(todo_id) in todos_dict.keys():
       return (todo_id, todos_dict[int(todo_id)])
     return todos_dict
